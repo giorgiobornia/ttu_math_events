8e848068a2bdadaaa1c787390d3f5e1728994d06
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-04-16 17:34:41 -0500

    Added favicon

--- AppliedMath/2018/fall/index.php
+++ AppliedMath/2018/fall/index.php
@@ -1,12 +1,14 @@
  <?php
 
- $institution = 'Texas Tech University';
- $department = 'Department of Mathematics and Statistics';
-
  $model_path = "../../../../seminars_model/"; //folder where the model class is
  
  include($model_path . "src/php/functions.php");
  
- Seminars::generate_seminar_page_by_topic($model_path, $institution, $department); 
+ $institution = 'Texas Tech University';
+ $department = 'Department of Mathematics and Statistics';
+ $toolbar_image = '../../../src/img/favicon.ico';
+  
+
+ Seminars::generate_seminar_page_by_topic($model_path, $institution, $department, $toolbar_image); 
  
  ?>
--- AppliedMath/2019/spring/index.php
+++ AppliedMath/2019/spring/index.php
@@ -1,12 +1,14 @@
  <?php
  
- $institution = 'Texas Tech University';
- $department = 'Department of Mathematics and Statistics';
-
  $model_path = "../../../../seminars_model/"; //folder where the model class is
  
  include($model_path . "src/php/functions.php");
+ 
+ $institution = 'Texas Tech University';
+ $department = 'Department of Mathematics and Statistics';
+ $toolbar_image = '../../../src/img/favicon.ico';
+  
 
- Seminars::generate_seminar_page_by_topic($model_path, $institution, $department); 
+ Seminars::generate_seminar_page_by_topic($model_path, $institution, $department, $toolbar_image); 
  
  ?>
