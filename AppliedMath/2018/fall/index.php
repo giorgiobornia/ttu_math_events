201cd30ee0ecb1793e2798d7eab9d0a075b7e313
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-04-16 23:26:35 -0500

    Passing discipline array from application

--- AppliedMath/2018/fall/index.php
+++ AppliedMath/2018/fall/index.php
@@ -1,18 +1,25 @@
  <?php
 
+ //library
  $model_path = "../../../../seminars_model/"; //folder where the model class is
  
  include($model_path . "./src/php/functions.php");
  
- $institution = 'Texas Tech University';
- $department = 'Department of Mathematics and Statistics';
- $icon_in_toolbar = '../../../src/img/favicon.ico';
+//application
+ include("../../../src/php/ttu_math_seminars.php");
  
  $topic = 'AppliedMath';
  $year = '2018';
  $semester = 'fall';
-  
 
- Seminars::generate_seminar_page_by_topic($model_path, $institution, $department,  $topic, $year, $semester, $icon_in_toolbar); 
+ 
+ Seminars::generate_seminar_page_by_topic($model_path, 
+                                          ttu_math_seminars::$institution, 
+                                          ttu_math_seminars::$department,  
+                                          $topic, 
+                                          $year, 
+                                          $semester,
+                                          ttu_math_seminars::$icon_in_toolbar,
+                                          ttu_math_seminars::$discipline_array); 
  
  ?>
--- by_week/index.php
+++ by_week/index.php
@@ -1,11 +1,14 @@
  <?php
  
+ //library
  $model_path = "../../seminars_model/"; //folder where the model class is
 
  include($model_path . "./src/php/functions.php");
 
+//application
  $title_in_toolbar = 'Seminars by week';
   $icon_in_toolbar = '../src/img/favicon.ico';
+            include('../src/php/ttu_math_seminars.php');
 
  $institution = 'Texas Tech University';
  $department = 'Department of Mathematics and Statistics';
@@ -17,6 +20,17 @@
  $month_end = 4;
  $day_end = 7;
 
- Seminars::generate_seminar_page_by_week($model_path, $institution, $department, $title_in_toolbar, $icon_in_toolbar, $year, $semester, $month_begin, $day_begin, $month_end, $day_end);
+ Seminars::generate_seminar_page_by_week($model_path, 
+                                         $institution,
+                                         $department,
+                                         $title_in_toolbar, 
+                                         $icon_in_toolbar, 
+                                         $year,
+                                         $semester, 
+                                         $month_begin, 
+                                         $day_begin, 
+                                         $month_end,
+                                         $day_end,
+                                         ttu_math_seminars::$discipline_array);
 
  ?>
