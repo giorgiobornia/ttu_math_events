d8d836a097ef6c049dd567179ec475692edcb600
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-04-17 08:08:02 -0500

    Added ttu_math_seminars.php

---
+++ src/php/ttu_math_seminars.php
@@ -0,0 +1,23 @@
+ <?php
+
+  class ttu_math_seminars {
+ 
+ public static  $institution = 'Texas Tech University';
+ public static  $department = 'Department of Mathematics and Statistics';
+ public static  $icon_in_toolbar = '../../../src/img/favicon.ico';
+
+ public static $discipline_array = array(
+ "AppliedMath"            => "Applied Mathematics",  ///@todo these second arguments CANNOT have SPACES, because they are used for some id below
+ "Analysis"               => "Analysis", 
+ 'AlgebraAndNumberTheory' => 'Algebra and Number Theory', 
+ 'Geometry'               => 'Geometry',
+ 'MathEd'                 => 'Mathematics Education',
+ 'RealAlgebraicGeometry'  => 'Real-Algebraic Geometry', 
+ 'Statistics'             => 'Statistics' 
+ );
+
+ }
+ 
+ 
+ ?>
+ 
