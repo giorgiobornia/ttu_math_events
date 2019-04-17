03892d326ce5a5a7f60316a3ddbd0afdbb2463d2
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-04-17 18:51:59 -0500

    Initialized all seminar folders

--- src/php/ttu_math_seminars.php
+++ src/php/ttu_math_seminars.php
@@ -7,17 +7,24 @@
  public static  $icon_in_toolbar = './src/img/favicon.ico';
 
  public static $discipline_array = array(
- "AppliedMath"            => "Applied Mathematics",  ///@todo these second arguments CANNOT have SPACES, because they are used for some id below
- "Analysis"               => "Analysis", 
  'AlgebraAndNumberTheory' => 'Algebra and Number Theory', 
+ "Analysis"               => "Analysis", 
+ "AppliedMath"            => "Applied Mathematics",
+ "Biomath"                => "Biomathematics",
  'Geometry'               => 'Geometry',
+ "ImageAnalysis"          => "Image Analysis",
+ 'LogicTopology'          => 'Logic-Topology',
+ 'MathClub'               => 'Math Club',
  'MathEd'                 => 'Mathematics Education',
+ 'PrepProfession'         => 'Preparation for the Profession',
  'RealAlgebraicGeometry'  => 'Real-Algebraic Geometry', 
- 'Statistics'             => 'Statistics' 
+ 'Statistics'             => 'Statistics', 
  );
 
  }
  
+ 
+ ///@todo write a function that checks that the directories of the inputs are there
  
  ?>
  
