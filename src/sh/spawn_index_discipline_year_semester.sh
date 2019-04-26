4fae096e5d57e0b2fbb82f7fa11666e9ce08a066
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-04-25 22:41:46 -0500

    Update an index file

--- src/sh/spawn_index_discipline_year_semester.sh
+++ src/sh/spawn_index_discipline_year_semester.sh
@@ -10,15 +10,15 @@ if test "$container" = ""; then
 echo "The second argument must be the directory (either relative or absolute) where all seminars are"; exit;
 fi
 
- cd $container
+ cd $container;
 
 
  for discipline in   algebra_and_number_theory  analysis  applied_math  biomath  geometry  image_analysis  logic_topology  math_club  math_ed   prep_for_profession  real_algebraic_geometry   statistics; 
  do
  cd $discipline/$year/$semester; 
 #  touch about.txt; 
-#   pwd;
-      cp ../../../src/sh/index_discipline_year_semester.php index.php
-#    ls -l ../../../src/sh/index_discipline_year_semester.php
+   pwd;
+#       cp ../../../src/sh/index_discipline_year_semester.php index.php;
+#    ls -l ../../../src/sh/index_discipline_year_semester.php;
  cd ../../../; 
  done
