121e297090ead30b155412bfbee98a0abea20c2f
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-05-14 13:47:12 -0500

    Cleaned past information folder, and created script to spawn a given semester

---
+++ src/sh/spawn_semester_files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ container=$1;
+ year=$2;
+ semester=$3;
+ 
+ 
+ 
+if test "$container" = ""; then
+echo "The second argument must be the directory (either relative or absolute) where all seminars are"; exit;
+fi
+
+ cd $container;
+
+
+ for discipline in   algebra_and_number_theory  analysis  applied_math  biomath  geometry  image_analysis  logic_topology  math_club  math_ed  prep_for_profession  real_algebraic_geometry  statistics  colloquia; 
+ do
+  mkdir -p $discipline/$year/$semester;
+  cd $discipline/$year/$semester;
+  cp -r ../../../src/sh/template_seminar_semester/.   .  #copies a folder without copying the containing folder as well
+  cd ../../../; 
+ done
+
+ 
+ echo "Remember to ACTIVATE the edition for each seminar"  # @todo see how to do that automatically: you have to take the existing file, see if the year is there, if so add the other semester, otherwise add a new line of the type 'year,semester'
