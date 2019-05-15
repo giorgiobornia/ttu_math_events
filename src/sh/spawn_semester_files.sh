0a536dace24c06ffbf4c4c754c652a43e02b7124
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-05-15 13:41:53 -0500

    Added all 2018 fall initialization files with a script, now fill and eventually activate

--- src/sh/spawn_semester_files.sh
+++ src/sh/spawn_semester_files.sh
@@ -13,13 +13,13 @@ fi
  cd $container;
 
 
- for discipline in   algebra_and_number_theory  analysis  applied_math  biomath  geometry  image_analysis  logic_topology  math_club  math_ed  prep_for_profession  real_algebraic_geometry  statistics  colloquia; 
- do
-  mkdir -p $discipline/$year/$semester;
-  cd $discipline/$year/$semester;
-  cp -r ../../../src/sh/template_seminar_semester/.   .  #copies a folder without copying the containing folder as well
-  cd ../../../; 
- done
+ for discipline in  algebra_and_number_theory   applied_math   analysis  biomath  geometry  image_analysis  logic_topology  math_club  math_ed  prep_for_profession  real_algebraic_geometry  statistics  colloquia;
+   do
+     mkdir -p $discipline/$year/$semester;
+     cd $discipline/$year/$semester;
+     cp -r ../../../src/sh/template_seminar_semester/.   .  # copies a folder without copying the containing folder as well
+     cd ../../../; 
+  done
 
  
  echo "Remember to ACTIVATE the edition for each seminar"  # @todo see how to do that automatically: you have to take the existing file, see if the year is there, if so add the other semester, otherwise add a new line of the type 'year,semester'
