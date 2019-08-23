c569a93ad9ad4efc60ed9fb70fdb5395020cb171
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-08-23 15:33:41 -0500

    Removed one link in the bar

--- seminars/applied_math/2019/spring/index.php
+++ seminars/applied_math/2019/spring/index.php
@@ -11,8 +11,10 @@
  include_once($relative_path_to_app . './src/php/ttu_math_seminars.php');
  
  $filename = __FILE__;
-  
- ttu_math_seminars::seminar_webpage($filename, $relative_path_to_app, $relative_path_to_lib, ttu_math_seminars::$seminar_container, ttu_math_seminars::$seminar_container, ttu_math_seminars::$colloquium_container);
+ 
+ $is_seminar_colloquium_all = 0;
+ 
+ ttu_math_seminars::seminar_webpage($filename, $relative_path_to_app, $relative_path_to_lib, $is_seminar_colloquium_all, ttu_math_seminars::$seminar_container, ttu_math_seminars::$colloquium_container);
  
  
  ?>
