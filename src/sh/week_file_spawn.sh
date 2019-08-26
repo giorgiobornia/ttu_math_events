3c27aaa8e1e98c1b5d7dd9ec9b402fac6751f34e
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-08-26 13:47:40 -0500

    Simplified a script

--- src/sh/week_file_spawn.sh
+++ src/sh/week_file_spawn.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#  ./week_file_spawn.sh ../../all/ 2019 spring
+#  ./week_file_spawn.sh ../../all/
 
 
 container=$1;
- year=$2;
- semester=$3;
  
  
 if test "$container" = ""; then
@@ -20,12 +18,16 @@ fi
 
 
 # # 2019, Spring 
- months=( 4  4  4 4 4  3  3  3 3  2  2  2 2  1  1);
-   days=(29 22 15 8 1 25 18 11 4 25 18 11 4 28 21);
+#  year=2019
+#  semester=spring
+#  months=( 4  4  4 4 4  3  3  3 3  2  2  2 2  1  1);
+#    days=(29 22 15 8 1 25 18 11 4 25 18 11 4 28 21);
 
 # 2019, Fall 
-#  months=( 8 9 9 9 9 9      10 10 10 10   11 11 11 11  12);
-#    days=(26 2 9 16 23 30   7 14 21 28    4 11 18 25    2);
+ year=2019
+ semester=fall
+ months=( 8 9 9 9 9 9      10 10 10 10   11 11 11 11  12);
+   days=(26 2 9 16 23 30   7 14 21 28    4 11 18 25    2);
 
 
  
