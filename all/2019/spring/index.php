5d2aa5fea252bcecec46b6e0e1ee009c3280765d
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-08-26 11:03:35 -0500

    Reading of active weeks now from csv file

--- all/2019/spring/index.php
+++ all/2019/spring/index.php
@@ -11,47 +11,17 @@ $relative_path_to_library = "../../../../colloquia_and_seminars_lib/";
  include($relative_path_to_app . './src/php/ttu_math_seminars.php');
  
  $icon_in_toolbar = $relative_path_to_app . ttu_math_seminars::$icon_in_toolbar;
+
+ $filename = __FILE__;
  
- $title = 'Colloquia and Seminars';
  
-
-  $first_monday_month = 1;
-  $first_monday_day = 21;
-  $last_monday_month = 4;
-  $last_monday_day = 29;
-  
-  
- $array_coords = Seminars::get_discipline_year_semester(__FILE__);
-    
- $semester = $array_coords[0];
- $year = $array_coords[1];
- $discipline = $array_coords[2];  //this will be 'all'
-    
-    
- $week_month_day_auto = Seminars::generate_initial_week_days($year, $first_monday_month, $first_monday_day, $last_monday_month, $last_monday_day);
-
-//to generate all semester files 
-//    Seminars::generate_initial_week_files($year, $first_monday_month, $first_monday_day, $last_monday_month, $last_monday_day,'../../../src/sh/week_file.php','./week/');
-
-   $is_seminar_colloquium_all = 2;
-
-    
- Seminars::generate_page_with_all_weeks_list($relative_path_to_library,
-                                             $title,
-                                             $icon_in_toolbar,
-                                             ttu_math_seminars::$math_server_url_base,
-                                             $relative_path_to_app,
-                                             ttu_math_seminars::$are_input_files_local,
-                                             $discipline,
-                                             ttu_math_seminars::$discipline_array,
-                                             ttu_math_seminars::$colloquium_array,
-                                             ttu_math_seminars::$seminar_container,
-                                             ttu_math_seminars::$colloquium_container,
-                                             $is_seminar_colloquium_all,
-                                             ttu_math_seminars::$department,
-                                             ttu_math_seminars::$institution,
-                                             $year,
-                                             $semester,
-                                             $week_month_day_auto); 
- ?>
+ Seminars::generate_page_with_all_weeks_list_wrapper($filename,
+                                                     $relative_path_to_library,
+                                                     $icon_in_toolbar,
+                                                     ttu_math_seminars::$math_server_url_base, $relative_path_to_app, ttu_math_seminars::$are_input_files_local,
+                                                     ttu_math_seminars::$discipline_array,  ttu_math_seminars::$colloquium_array,
+                                                     ttu_math_seminars::$seminar_container, ttu_math_seminars::$colloquium_container,
+                                                     ttu_math_seminars::$department,
+                                                     ttu_math_seminars::$institution);
  
+?>
