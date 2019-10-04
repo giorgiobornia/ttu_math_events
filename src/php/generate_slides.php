1601eab96f535fb7831c9d50b8a3f5b64e2d3e70
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-10-04 16:35:12 -0500

    First call to a function that generates .tex files and then compiles to pdf files

---
+++ src/php/generate_slides.php
@@ -0,0 +1,31 @@
+  <?php
+  
+  
+  require './ttu_math_seminars.php'; 
+
+  $relative_path_to_apps = '../../'; 
+$is_seminar_colloquium_all = 2; 
+
+
+$year = 2019;
+$semester = 'spring';
+$month_begin = 10;
+$day_begin = 7;
+$month_end = 10;
+$day_end = 14;
+
+Seminars::generate_pdf_slides_by_time_range(ttu_math_seminars::$math_server_url_base,
+                                            $relative_path_to_apps,
+                                            ttu_math_seminars::$are_input_files_local,
+                                            ttu_math_seminars::$institution,
+                                            ttu_math_seminars::$department,
+                                            $year, $semester, $month_begin, $day_begin, $month_end, $day_end, 
+                                            ttu_math_seminars::$discipline_array,
+                                            ttu_math_seminars::$colloquium_array,
+                                            ttu_math_seminars::$seminar_container,
+                                            ttu_math_seminars::$colloquium_container,
+                                            $is_seminar_colloquium_all
+        );
+  
+  
+  ?>
