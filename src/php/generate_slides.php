eb8f80f74006b17b35a6c7358b533186aee8141b
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-10-04 19:13:59 -0500

    First loop over seminars

--- src/php/generate_slides.php
+++ src/php/generate_slides.php
@@ -1,14 +1,13 @@
   <?php
   
-  
+//- run this script automatically every Friday afternoon
+
   require './ttu_math_seminars.php'; 
 
   $relative_path_to_apps = '../../'; 
-$is_seminar_colloquium_all = 2; 
+  
 
 
-$year = 2019;
-$semester = 'spring';
 $month_begin = 10;
 $day_begin = 7;
 $month_end = 10;
@@ -19,13 +18,11 @@ Seminars::generate_pdf_slides_by_time_range(ttu_math_seminars::$math_server_url_
                                             ttu_math_seminars::$are_input_files_local,
                                             ttu_math_seminars::$institution,
                                             ttu_math_seminars::$department,
-                                            $year, $semester, $month_begin, $day_begin, $month_end, $day_end, 
+                                            ttu_math_seminars::$current_year, ttu_math_seminars::$current_semester, $month_begin, $day_begin, $month_end, $day_end, 
                                             ttu_math_seminars::$discipline_array,
                                             ttu_math_seminars::$colloquium_array,
                                             ttu_math_seminars::$seminar_container,
-                                            ttu_math_seminars::$colloquium_container,
-                                            $is_seminar_colloquium_all
-        );
+                                            ttu_math_seminars::$colloquium_container);
   
   
   ?>
