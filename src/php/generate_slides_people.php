d2e8db28301f2c607e9f00a825eabd41f277da65
Giorgio Bornia <giorgio.bornia@ttu.edu>
2020-01-24 17:26:34 -0600

    Fix the compilation errors by modifying the database entries

--- src/php/generate_slides_people.php
+++ src/php/generate_slides_people.php
@@ -4,17 +4,31 @@
 // include($_SERVER['DOCUMENT_ROOT']."includes/people.php");
 include("../../../events_lib/src/php/functions.php");
 
+ // database server info -----------
 $dsn      = "mysql:host=webwork.math.ttu.edu; dbname=deptweb";
 $username = "readdeptweb";
 $password = "byrt82fw";
 
-  echo Events::render('Control', $dsn, $username, $password);
+ 
+ // connect to database -----------
+	try {
+			$conn = new PDO ($dsn, $username, $password );
+			$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	} catch ( PDOException $e) {
+			echo  "Connection failed: " . $e->getMessage();
+	}
+
+
+// query -----------
+   $name_first = "Giorgio";
+   $name_last  = "Bornia";
   
-  $name_first = "Giorgio";
-  $name_last  = "Bornia";
-  $bio = "Hello world";
-  $image_name = "blank_face.png";
+   $sql = "SELECT * FROM roster WHERE Last_Name='" . $name_last . "' AND First_Name='" . $name_first . "'";
+//   $sql = "SELECT * FROM roster WHERE Employed='T' AND Rank<'4' ORDER BY LAST_NAME, FIRST_NAME";  ///@todo see how to randomize over all faculty //see how to avoid Latex compile errors for & and so on
+
+   $rows = $conn->query( $sql );
+   
   
-  Events::single_latex_pdf_slide_person($dsn, $username, $password, $name_first, $name_last, $bio, $image_name);
+  Events::single_latex_pdf_slide_person($rows);
 
 ?>
