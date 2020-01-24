729cf5bd21621da522361c845a15f7c6d88cb09d
Giorgio Bornia <giorgio.bornia@ttu.edu>
2020-01-24 15:35:07 -0600

    Skeleton of people slide working

--- src/php/generate_slides_people.php
+++ src/php/generate_slides_people.php
@@ -8,8 +8,8 @@ $dsn = "mysql:host=webwork.math.ttu.edu; dbname=deptweb";
 $username = "readdeptweb";
 $password = "byrt82fw";
 
+//   echo Events::render('Control', $dsn, $username, $password);
 //   echo render('Analysis', $dsn, $username, $password);
-  echo Events::render('Control', $dsn, $username, $password);
 //   echo render('all', $dsn, $username, $password);
 //   echo render('grad', $dsn, $username, $password); 
 //   echo render('office', $dsn, $username, $password); 
@@ -22,5 +22,12 @@ $password = "byrt82fw";
 //   echo render('instruct_lect', $dsn, $username, $password); 
 //   echo render('former', $dsn, $username, $password); 
 //   echo render('advising', $dsn, $username, $password); 
+  
+  $name_first = "Giorgio";
+  $name_last  = "Bornia";
+  $bio = "Hello world";
+  $image_name = "blank_face.png";
+  
+  Events::single_latex_pdf_slide_person($name_first, $name_last, $bio, $image_name);
 
 ?>
