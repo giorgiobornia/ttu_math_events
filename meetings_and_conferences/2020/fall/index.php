133106cd0f688f57da50b2b3615fba7665f76a50
Giorgio Bornia <giorgio.bornia@ttu.edu>
2020-01-31 12:00:39 -0600

    Added images and some events' info

---
+++ meetings_and_conferences/2020/fall/index.php
@@ -0,0 +1,19 @@
+ <?php
+ 
+ //library
+ $relative_path_to_lib = "../../../../events_lib/";
+
+ include_once($relative_path_to_lib . "./src/php/functions.php");  //this is the include that works
+
+ //application-specific
+ $relative_path_to_app = '../../../';
+
+ include_once($relative_path_to_app . './src/php/ttu_math_events.php');
+ 
+ $filename = __FILE__;
+ 
+ ttu_math_events::topic_webpage($filename, 
+                                  $relative_path_to_app, $relative_path_to_lib);
+ 
+ 
+ ?>
